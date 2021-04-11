11144671bc4c6fb9ef4a934de399fecd02f504d8
TiagoSeixas2103 <tvseixas@gmail.com>
2021-04-11 18:34:45 -0300

    adicionando Register8

--- Projetos/D-LogicaSequencial/src/Register8.vhd
+++ Projetos/D-LogicaSequencial/src/Register8.vhd
@@ -27,5 +27,13 @@ architecture arch of Register8 is
 
 begin
 
+O0 : BinaryDigit port map (clock, input(0), load, output(0));
+O1 : BinaryDigit port map (clock, input(1), load, output(1));
+O2 : BinaryDigit port map (clock, input(2), load, output(2));
+O3 : BinaryDigit port map (clock, input(3), load, output(3));
+O4 : BinaryDigit port map (clock, input(4), load, output(4));
+O5 : BinaryDigit port map (clock, input(5), load, output(5));
+O6 : BinaryDigit port map (clock, input(6), load, output(6));
+O7 : BinaryDigit port map (clock, input(7), load, output(7));
 
 end architecture;
