8abcd6bee1151c6c4d759ee0cf71e53c752fe501
NicolasCho <nicolasbkc@gmail.com>
2021-04-22 18:00:50 -0300

    resolve counterdown do conceito A

--- Projetos/D-LogicaSequencial/testes/tb_CounterDown.vhd
+++ Projetos/D-LogicaSequencial/testes/tb_CounterDown.vhd
@@ -34,8 +34,38 @@ begin
     test_runner_setup(runner, runner_cfg);
 
     -- IMPLEMENTE AQUI!
+
+    --7
     wait until clk'event and clk='0';
-		assert(Q = "111")  report "Precisa fazer os testes" severity error;
+		assert(q = "111")  report "Precisa fazer os testes" severity error;
+
+    --6
+    wait until clk'event and clk='0';
+		assert(q = "110")  report "Precisa fazer os testes" severity error;
+
+    --5
+    wait until clk'event and clk='0';
+		assert(q = "101")  report "Precisa fazer os testes" severity error;
+
+    --4
+    wait until clk'event and clk='0';
+		assert(q = "100")  report "Precisa fazer os testes" severity error;
+
+    --3
+    wait until clk'event and clk='0';
+		assert(q = "011")  report "Precisa fazer os testes" severity error;
+
+    --2
+    wait until clk'event and clk='0';
+		assert(q = "010")  report "Precisa fazer os testes" severity error;
+
+    --1
+    wait until clk'event and clk='0';
+		assert(q = "001")  report "Precisa fazer os testes" severity error;
+
+    --0
+    wait until clk'event and clk='0';
+		assert(q = "000")  report "Precisa fazer os testes" severity error;
 
 
     -- finish
